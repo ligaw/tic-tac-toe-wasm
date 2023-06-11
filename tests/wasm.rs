9f7f9a495f887a95ce7225d7b5ecc5faf47a629d
Pablo Puyat <pablo@cbrz.com>
2023-06-10 20:06:01 -0400

    Made tests a module. Naming test files after the modules they contain

---
+++ tests/wasm.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod wasm {
+    wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+    use wasm_bindgen_test::wasm_bindgen_test;
+    use tic_tac_toe::GameWrapper;
+    use tic_tac_toe::Player;
+
+    #[wasm_bindgen_test]
+    pub fn test_move() {
+        assert_eq!(GameWrapper::new().current_player(), Player::X);
+    }
+}
