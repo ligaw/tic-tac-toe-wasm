b4ac076bf347834e6308d30c12bf54c50a7b70e2
Pablo Puyat <pablo@cbrz.com>
2023-06-16 21:54:31 -0400

    Simple board using Vue and tailwind

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,30 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 h-screen">
+  <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+  <div class="mx-auto max-w-3xl h-full">
+    <Board />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ www/src/components/Board.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="board" class="grid grid-cols-3 gap-3 h-full bg-amber-700">
+    <div>0,0</div>
+    <div>0,1</div>
+    <div>0,2</div>
+    <div>1,0</div>
+    <div>1,1</div>
+    <div>1,2</div>
+    <div>2,0</div>
+    <div>2,1</div>
+    <div>2,2</div>
+  </div>
+</template>
+
+<style scoped>
+#board div {
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  place-items: center;
+}
+</style>
