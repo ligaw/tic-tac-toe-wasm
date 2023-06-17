01d3d737c940781e7e77a384f57f60b70686f0a5
Pablo Puyat <pablo@cbrz.com>
2023-06-16 20:42:28 -0400

    Added checkhealth

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::prelude::*;
+
 mod game_state;
 mod game_wrapper;
 mod player;
@@ -5,3 +7,9 @@ mod player;
 pub use game_state::GameState;
 pub use game_wrapper::GameWrapper;
 pub use player::Player;
+
+
+#[wasm_bindgen]
+pub fn healthcheck() -> String {
+    "OK".to_string()
+}
