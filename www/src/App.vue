910aee2983968c42e820e872e74cc0a7985ad2e2
Pablo Puyat <pablo@cbrz.com>
2023-06-18 20:41:32 -0400

    Initialize WASM on mount of App

--- www/src/App.vue
+++ www/src/App.vue
@@ -2,12 +2,12 @@
 import Board from '@/components/Board.vue'
 import { useWasmStore } from '@/stores/store.js'
 import { onMounted, onDeactivated } from 'vue'
-//import * as wasm from 'wasm'
 
 onMounted(() => {
   console.log("mounting module")
-  //console.log(wasm.healthcheck())
-  //console.log("mounted was module")
+  const store = useWasmStore()
+  store.initializeWasm()
+  console.log("mounted was module")
 });
 
 onDeactivated(() => {
@@ -18,7 +18,6 @@ onDeactivated(() => {
 <template>
 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 h-screen">
   <div class="mx-auto max-w-3xl h-full">
-    Hello
     <Board />
   </div>
 </div>
--- www/src/stores/store.js
+++ www/src/stores/store.js
@@ -1,21 +1,19 @@
 import { defineStore } from 'pinia'
-//import * as wasm from 'wasm'
+import * as wasm from 'wasm'
 
 export const useWasmStore = defineStore('wasm', () => {
-  //const count = ref(0)
-  //const name = ref('Eduardo')
-  //const doubleCount = computed(() => count.value * 2)
-
-  function makeMove(column, row) {
-    //const result = wasm.make_move(column, row)
-    console.log("Made Move")
-  }
-/*
   function initializeWasm() {
     console.log("initializing wasm")
-    const blah = wasm.healtchech()    
+    const blah = wasm.healthcheck()    
     console.log(blah)
   }
-*/
-  return { makeMove }
+
+  function makeMove(column, row) {
+    const result = wasm.make_move(column, row)
+    console.log(result)
+    console.log("Made Move")
+  }
+
+
+  return { initializeWasm, makeMove }
 })
